635b21511a993ec9ae723a22114e65501e9e2f21
Robert-RKKR <64611240+Robert-RKKR@users.noreply.github.com>
2022-01-14 18:57:35 +0100

    BIGUPDATE - All models updated to BaseModel (BaseMainModel and BaseSubModel).

--- backend/autocli/settings.py
+++ backend/autocli/settings.py
@@ -114,18 +114,12 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        #'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
-    # 'DEFAULT_RENDERER_CLASSES': (
-    #     'rest_framework.renderers.JSONRenderer',
-    # ),
-    #'USER_DETAILS_SERIALIZER':'administration.serializer.AdministratorDetailsSerializer'
 }
 
 SWAGGER_SETTINGS = {
